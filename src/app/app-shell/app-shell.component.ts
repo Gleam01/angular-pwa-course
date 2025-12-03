7541b7c3026841954dd24a5bd8b51de748a8053e
Yassr HADJ ABDOU RAZACK <yassrhadj@gmail.com>
2025-12-03 17:28:50 +0100

    "Angular Progressive Web Apps (PWA) MasterClass & FREE E-Book" course sections 1, 2 and 3 scripts added

---
+++ src/app/app-shell/app-shell.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shell',
+  template: `
+  <img class="loading-indicator" src="/assets/loading.gif" alt="Loading..." />
+  `,
+  styles: [`
+    .loading-indicator {
+      height: 300px;
+      margin: 0 auto;
+    }
+    `]
+})
+export class AppShellComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
