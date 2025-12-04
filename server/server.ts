0c6c9fd6c811b11333e7e724d256db406531ba4f
Yassr HADJ ABDOU RAZACK <yassrhadj@gmail.com>
2025-12-04 12:10:48 +0100

    "Angular Progressive Web Apps (PWA) MasterClass & FREE E-Book" Notification section codebase added

--- server/server.ts
+++ server/server.ts
@@ -1,60 +1,38 @@
+import * as express from "express";
+import { Application } from "express";
+import { readAllLessons } from "./read-all-lessons.route";
+import { addPushSubscriber } from "./add-push-subscriber.route";
+import { sendNewsletter } from "./send-newsletter.route";
+const bodyParser = require("body-parser");
 
-import * as express from 'express';
-import {Application} from "express";
-import {readAllLessons} from "./read-all-lessons.route";
-import {addPushSubscriber} from "./add-push-subscriber.route";
-import {sendNewsletter} from "./send-newsletter.route";
-const bodyParser = require('body-parser');
-
-const webpush = require('web-push');
-
+const webpush = require("web-push");
 
 const vapidKeys = {
-    publicKey: "TODO",
-    privateKey: "TODO"
+  publicKey:
+    "BAqgNe-odlLiEQBthpI3XQj7BQ3lTSmqVhMlNL_NyH9mFkCRcrUpvADZPbO6RtJd1lVy2wN8r-GZfK3ssH1zQzk",
+  privateKey: "bzhPCUjowKodc2u0o-qUSkAVHSfX3k8SfSFVe6BR0nw",
 };
 
-/*
-
-    TODO - uncomment after generating your VAPID keys
-
 webpush.setVapidDetails(
-    'mailto:example@yourdomain.org',
-    vapidKeys.publicKey,
-    vapidKeys.privateKey
+  "mailto:example@yourdomain.org",
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
 );
-
-*/
-
 
 const app: Application = express();
 
-
 app.use(bodyParser.json());
 
-
 // REST API
-app.route('/api/lessons')
-    .get(readAllLessons);
+app.route("/api/lessons").get(readAllLessons);
 
-app.route('/api/notifications')
-    .post(addPushSubscriber);
+app.route("/api/notifications").post(addPushSubscriber);
 
-app.route('/api/newsletter')
-    .post(sendNewsletter);
-
-
+app.route("/api/newsletter").post(sendNewsletter);
 
 // launch an HTTP Server
-const httpServer:any = app.listen(9000, () => {
-    console.log("HTTP Server running at http://localhost:" + httpServer.address().port);
+const httpServer: any = app.listen(9001, () => {
+  console.log(
+    "HTTP Server running at http://localhost:" + httpServer.address().port
+  );
 });
-
-
-
-
-
-
-
-
-
--- src/app/lessons/lessons.component.ts
+++ src/app/lessons/lessons.component.ts
@@ -1,52 +1,70 @@
-import {Component, OnInit} from '@angular/core';
-import {LessonsService} from "../services/lessons.service";
-import {Observable, of} from 'rxjs';
-import {Lesson} from "../model/lesson";
-import {SwPush} from "@angular/service-worker";
-import {NewsletterService} from "../services/newsletter.service";
-import {catchError} from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { LessonsService } from "../services/lessons.service";
+import { Observable, of } from "rxjs";
+import { Lesson } from "../model/lesson";
+import { SwPush } from "@angular/service-worker";
+import { NewsletterService } from "../services/newsletter.service";
+import { catchError } from "rxjs/operators";
 
 @Component({
-    selector: 'lessons',
-    templateUrl: './lessons.component.html',
-    styleUrls: ['./lessons.component.css']
+  selector: "lessons",
+  templateUrl: "./lessons.component.html",
+  styleUrls: ["./lessons.component.css"],
 })
 export class LessonsComponent implements OnInit {
+  lessons$: Observable<Lesson[]>;
+  isLoggedIn$: Observable<boolean>;
+  sub: PushSubscription;
 
-    lessons$: Observable<Lesson[]>;
-    isLoggedIn$: Observable<boolean>;
+  readonly VAPID_PUBLIC_KEY =
+    "BAqgNe-odlLiEQBthpI3XQj7BQ3lTSmqVhMlNL_NyH9mFkCRcrUpvADZPbO6RtJd1lVy2wN8r-GZfK3ssH1zQzk";
 
-    readonly VAPID_PUBLIC_KEY = "TODO";
+  constructor(
+    private lessonsService: LessonsService,
+    private newsletterService: NewsletterService,
+    private swPush: SwPush
+  ) {}
 
-    constructor(
-        private lessonsService: LessonsService,
-        private newsletterService: NewsletterService) {
+  ngOnInit() {
+    this.loadLessons();
+  }
 
-    }
+  loadLessons() {
+    this.lessons$ = this.lessonsService
+      .loadAllLessons()
+      .pipe(catchError((err) => of([])));
+  }
 
-    ngOnInit() {
-        this.loadLessons();
-    }
+  subscribeToNotifications() {
+    this.swPush
+      .requestSubscription({
+        serverPublicKey: this.VAPID_PUBLIC_KEY,
+      })
+      .then((sub) => {
+        console.log("Notification Subscription:", sub);
+        this.sub = sub;
+        this.newsletterService.addPushSubscriber(sub).subscribe(
+          (res) => {
+            console.log("Sent push subscription to server: ", res);
+          },
+          (err) => {
+            console.log("Failed to send push subscription to server: ", err);
+          }
+        );
+      })
+      .catch((err) =>
+        console.error("Could not subscribe to notifications", err)
+      );
+  }
 
-
-    loadLessons() {
-        this.lessons$ = this.lessonsService.loadAllLessons().pipe(catchError(err => of([])));
-    }
-
-    subscribeToNotifications() {
-
-
-
-    }
-
-
-    sendNewsletter() {
-
-
-    }
-
-
-
-
-
+  sendNewsletter() {
+    this.newsletterService.send().subscribe(
+      (res) => {
+        console.log("Newsletter sent: ", res);
+      },
+      (err) => {
+        console.log("Failed to send newsletter: ", err);
+      }
+    );
+  }
 }
